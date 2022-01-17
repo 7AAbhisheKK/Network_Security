1377488bf1e1fc122b8bd1378c25565cfc6f2d1e
Abhishek Raj <ar23072000@gmail.com>
2022-01-17 21:45:01 +0530

    PA0 Commented

--- PA0/encryption/views.py
+++ PA0/encryption/views.py
@@ -10,14 +10,14 @@ def home(request):
         encryption = True
         if 'cypher' in request.POST:
             for x in text1:
-                temp = temp + chr(25-ord(x)+2*ord('a'))
+                temp = temp + chr(25-ord(x)+2*ord('a'))   #converting to the Encipher text
             print(temp)
         if 'decypher' in request.POST:
             for x in text1:
-                temp = temp + chr(25-ord(x)+2*ord('a'))
+                temp = temp + chr(25-ord(x)+2*ord('a'))  #Deciphering the text
             print(temp)
             encryption = False
        
-        return render(request, 'result.html',{"result":temp,"encryption":encryption})
+        return render(request, 'result.html',{"result":temp,"encryption":encryption})  # returning the result page
     else:
         return render(request, 'home.html',{"message":"hello"})
